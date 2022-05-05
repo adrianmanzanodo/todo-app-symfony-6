5d0c061b8cd857793c338cdc978de9e990625c6c
Adrian Manzano <adrian.manzao.do@gmail.com>
2022-05-05 16:47:28 +0200

    WIP index view

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -7,22 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\Type\TaskType;
 use App\Entity\Task;
+use App\Repository\TaskRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 
 
 class TaskController extends AbstractController
 {
+    private $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/', name: 'task_index')]
     public function index(): Response
     {
+
+        $taskRepository = $this->doctrine->getRepository(Task::class);
+        $tasks = $taskRepository->findAll();
+
         return $this->render('task/index.html.twig', [
-            'message' => 'Welcome to index controller!',
-            'tasks' => [],
+            'tasks' => $tasks,
         ]);
     }
 
-    #[Route('/task/create', name:'task_create')]
+    #[Route('/task/create', name: 'task_create')]
     public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $task = new Task();
